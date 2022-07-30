ac6e187adc6baad88230d0c8162c8c4b99ed8805
al.abir313@gmail.com <AbirAlFoysal>
2022-07-30 20:51:59 +0600

    auto create customer for new user

--- store/models.py
+++ store/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-
+from django.db.models.signals import post_save
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default='Emon')
-    name = models.CharField(max_length=50, default="Emon")
-    email = models.EmailField()
-    def __str__(self):
-        return self.name
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True)
+    name = models.CharField(max_length=50,default="Emon")
+    email = models.EmailField(blank=True)
+    # def __str__(self):
+    #     return self.name
+
+
+def create_customer(sender, instance, created, *args, **kwargs):
+    if created:
+        Customer.objects.create(user = instance)
+post_save.connect(create_customer, sender = User)
+
 
 class Product(models.Model):
     name =  models.CharField(max_length=50)
